02482aa6bf016a0a1613f6e9db2bdad316a2e8f1
beauhodes <beauhodes@gmail.com>
2022-09-09 17:48:57 -0500

    add axios v2

--- src/pages/MintMain/components/MintSection.jsx
+++ src/pages/MintMain/components/MintSection.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import styled from "styled-components";
+import axios from 'axios';
 
 import { connect } from "redux/blockchain/blockchainActions";
 import { fetchDataBase } from "redux/data/dataActions";
